0e0ed717e49d4f5dcc6bd36c1017895a839474e8
Nikita Smelov <smelovn@gmail.com>
2015-05-13 22:11:00 +0300

    init

--- src/field.rs
+++ src/field.rs
@@ -1,11 +1,38 @@
 
 pub struct Field{
     state: u32,
-    right: Option<Box<Field>>,
+    left: Option<Box<Field>>,
+    pub right: Option<Box<Field>>,
+    up: Option<Box<Field>>,
     down: Option<Box<Field>>,
+}
+
+impl Field{
+    fn new(state: u32) -> Field {
+        Field {
+            state: state,
+            left: None,
+            right: None,
+            up: None,
+            down: None,
+        }
+    }
 }
 
 
 pub struct Map{
     first: Option<Field>,
+}
+
+impl Map{
+    fn new(x: u32, y: u32) -> Map {
+        let mut map = Map{first: Some(Field::new(0))};
+        for i in 0..x {
+            let mut field = map.first.unwrap();
+            for j in 0..y {
+                field.right = Some(Box::new(Field::new(0)));
+                field = field.right.unwrap();
+            }
+        }
+    }
 }
